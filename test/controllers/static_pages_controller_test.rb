57f6e7d6d024268615fc95572b5c988f62579e2d
sriharsha67 <sriharsha67@gmail.com>
2015-07-22 00:27:10 +0530

    Add a full_file helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,6 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
