ffe51b961722071fd8508130e2a8d526627be73e
sriharsha67 <sriharsha67@gmail.com>
2015-07-22 22:35:50 +0530

    Integration test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  get 'help'      => 'static_pages#help'
+  get 'about'    => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
