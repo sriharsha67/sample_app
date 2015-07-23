96b7a9dc20149313898f1e72fabcae7b60d7a341
sriharsha67 <sriharsha67@gmail.com>
2015-07-24 02:46:41 +0530

    Finish User Signup

--- db/schema.rb
+++ db/schema.rb
@@ -21,4 +21,6 @@ ActiveRecord::Schema.define(version: 20150723084507) do
     t.string   "password_digest"
   end
 
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+
 end
